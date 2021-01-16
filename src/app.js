8bd86a3f9841f6233d8562906023c4a89be79dc6
thomas.martin <thomas.martin@neo9.fr>
2021-01-16 08:09:24 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,48 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "http://localhost:8100",
+    methods: ["GET", "POST"]
+  }
+});
+
+const boards = {};
+
+io.on('connection', (socket) => {
+  let previousId;
+  const safeJoin = (currentId) => {
+    console.log('safeJoin');
+    // socket.leave(previousId);
+    // socket.join(currentId);
+    // previousId = currentId;
+  };
+
+  socket.on('get', (id) => {
+    console.log('get');
+    // safeJoin(id);
+    // socket.emit('board', boards[id]);
+  });
+
+  socket.on("add", b => {
+    console.log('add');
+    // boards[b.id] = b;
+    // safeJoin(b.id);
+    // io.emit("boards", Object.keys(boards));
+    // socket.emit("board", b);
+  });
+
+  socket.on("edit", b => {
+    console.log('edit');
+    // boards[b.id] = b;
+    // socket.to(b.id).emit("board", b);
+  });
+
+  // io.emit('boards', Object.keys(boards));
+
+  console.log(`Socket ${socket.id} has connected`);
+});
+
+http.listen(4444, () => {
+  console.log('Listening on port 4444');
+});
