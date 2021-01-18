22243f8ab6ab726d3a322e180d758d9ddbab8ef1
thomas.martin <thomas.martin@neo9.fr>
2021-01-18 15:52:48 +0100

    moving to a multiplayer game, warmful changes

--- src/app.js
+++ src/app.js
@@ -10,36 +10,66 @@ const io = require('socket.io')(http, {
 const boards = {};
 
 io.on('connection', (socket) => {
-  let previousId;
-  const safeJoin = (currentId) => {
-    console.log('safeJoin');
-    socket.leave(previousId);
-    socket.join(currentId);
-    previousId = currentId;
+  const generateName = () => {
+    const adjs = [
+      'Fancy',
+      'Terrifying',
+      'Amazing',
+      'True',
+      'Fake',
+      'Dark',
+      'Divine',
+      'Half',
+    ];
+    const noms = [
+      'Unicorn',
+      'Mage',
+      'Troll',
+      'Goblin',
+      'Genuis',
+      'Asshole',
+      'Elf',
+      'Dwarf',
+    ];
+
+    return (
+      adjs[Math.floor(Math.random() * adjs.length)] +
+      '' +
+      noms[Math.floor(Math.random() * noms.length)]
+    );
   };
 
-  socket.on('get', (id) => {
-    console.log('get');
-    safeJoin(id);
-    socket.emit('board', boards[id]);
+  socket.on('add', (b) => {
+    b.player.name = b.player.name ? b.player.name : generateName();
+
+    console.log(
+      'Welcome',
+      b.player.name,
+      '! Your hand is:',
+      b.hand.map((c) => c.name)
+    );
+    boards[socket.id] = b;
+
+    //io emits to all users
+    io.emit('boards', Object.values(boards));
+    //socket emit to the user calling the event
+    socket.emit('you', b.player.name);
   });
 
-  socket.on("add", b => {
-    console.log('add; your hand is:', b.hand.map(c => c.name));
-    boards[b.id] = b;
-    safeJoin(b.id);
-    io.emit("boards", Object.keys(boards));
-    socket.emit("board", b);
-  });
-
-  socket.on("edit", b => {
+  socket.on('edit', (b) => {
     console.log('edit');
-    boards[b.id] = b;
-    // socket.to(b.id).emit("board", b);
-    socket.emit("board", b);
+    boards[socket.id] = b;
+
+    //io emits to all users
+    io.emit('boards', Object.values(boards));
+  });
+  
+  socket.on('disconnect', function () {
+    console.log('Got disconnect!');
+    if (boards[socket.id]) delete boards[socket.id];
   });
 
-  io.emit('boards', Object.keys(boards));
+  io.emit('boards', Object.values(boards));
 
   console.log(`Socket ${socket.id} has connected`);
 });
