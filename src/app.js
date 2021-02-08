2b277561b0c2e4848509f4ca54548fcbe5f055f4
thomas.martin <thomas.martin@neo9.fr>
2021-02-08 09:31:10 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -11,88 +11,47 @@ setInterval(() => {
   io.emit('time', new Date().toTimeString());
 }, 1000);
 
-const boards = {};
+const players = {};
 
 io.on('connection', (socket) => {
-  const generateName = () => {
-    const adjs = [
-      'Fancy',
-      'Terrifying',
-      'Amazing',
-      'True',
-      'Fake',
-      'Dark',
-      'Divine',
-      'Half',
-      'Headless',
-      'Sneaky',
-      'A Fucking',
-    ];
-    const noms = [
-      'Unicorn',
-      'Kitten',
-      'Mage',
-      'Troll',
-      'Goblin',
-      'Genuis',
-      'Asshole',
-      'Dickhead',
-      'Elf',
-      'Dwarf',
-      'Goat',
-    ];
-
-    return (
-      adjs[Math.floor(Math.random() * adjs.length)] +
-      ' ' +
-      noms[Math.floor(Math.random() * noms.length)]
-    );
-  };
-
-  socket.on('add', (b) => {
-    b.player.id = socket.id;
-    b.player.name = b.player.name ? b.player.name : generateName();
-
-    console.log('Welcome', b.player.name);
-    boards[socket.id] = b;
+  socket.on('add', (name) => {
+    console.log('Welcome', name);
+    players[socket.id] = {
+      id: socket.id,
+      name: name,
+      hand: null, // NEEDS TO BE CHANGED TO STARTING HAND
+    };
 
     //io emits to all users
-    io.emit('boards', Object.values(boards));
+    io.emit('players', Object.values(players));
     //socket emit to the user calling the event
-    socket.emit('you', b.player.name);
+    // socket.emit('you', name);
   });
 
-  socket.on('edit', (b) => {
-    boards[socket.id] = b;
+  // socket.on('target', ({ cardID, playerID }) => {
+  //   if (players[playerID]) {
+  //     const targetIndex = players[playerID].targettedCards.findIndex(
+  //       (CID) => CID == cardID
+  //     );
+  //     targetIndex >= 0
+  //       ? players[playerID].targettedCards.splice(targetIndex, 1)
+  //       : players[playerID].targettedCards.push(cardID);
 
-    //io emits to all users
-    io.emit('boards', Object.values(boards));
-  });
-
-  socket.on('target', ({ cardID, playerID }) => {
-    if (boards[playerID]) {
-      const targetIndex = boards[playerID].targettedCards.findIndex(
-        (CID) => CID == cardID
-      );
-      targetIndex >= 0
-        ? boards[playerID].targettedCards.splice(targetIndex, 1)
-        : boards[playerID].targettedCards.push(cardID);
-
-      //io emits to all users
-      io.emit('boards', Object.values(boards));
-    }
-  });
+  //     //io emits to all users
+  //     io.emit('players', Object.values(players));
+  //   }
+  // });
 
   socket.on('disconnect', function () {
     console.log('Got disconnect!');
-    if (boards[socket.id]) delete boards[socket.id];
+    if (players[socket.id]) delete players[socket.id];
 
     //io emits to all users
-    io.emit('boards', Object.values(boards));
+    io.emit('players', Object.values(players));
   });
 
   //io emits to all users
-  io.emit('boards', Object.values(boards));
+  io.emit('players', Object.values(players));
 
   console.log(`Socket ${socket.id} has connected`);
 });
